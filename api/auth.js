6fcb673897356a6f5285f97350d77fa07642cb87
luizpbello <luizpaulopinto25@gmail.com>
2022-08-18 11:46:36 -0300

    authsecret

--- api/auth.js
+++ api/auth.js
@@ -1,4 +1,4 @@
-const { authSecret } = require("../.env");
+//const { authSecret } = require("../.env");
 const jwt = require("jwt-simple");
 const bcrypt = require("bcrypt-nodejs");
 
@@ -31,7 +31,7 @@ module.exports = (app) => {
 
     res.json({
       ...payload,
-      token: jwt.encode(payload, authSecret),
+      token: jwt.encode(payload, process.env.authSecret),
     });
   };
 
@@ -39,7 +39,7 @@ module.exports = (app) => {
     const userData = req.body || null;
     try {
       if (userData) {
-        const token = jwt.decode(userData.token, authSecret);
+        const token = jwt.decode(userData.token, process.env.authSecret);
         if (new Date(token.exp * 1000 > new Date())) {
           return res.send(true);
         }
