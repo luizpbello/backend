37d9368cf9b7d37db426ce287ec104868bb3fb93
luizpbello <luizpaulopinto25@gmail.com>
2022-08-18 11:18:44 -0300

    new envs

--- knexfile.js
+++ knexfile.js
@@ -4,19 +4,16 @@
  * @type { Object.<string, import("knex").Knex.Config> }
  */
 module.exports = {
-    client: 'postgresql',
-    connection: {
-      database: 'system',
-      user:     'postgres',
-      password: 'biriba22'
-    },
-    pool: {
-      min: 2,
-      max: 10
-    },
-    migrations: {
-      tableName: 'knex_migrations'
-    }
-  
-
+  client: "postgresql",
+  connection: {
+    connectionString: process.env.DATABASE_URL,
+    ssl: { rejectUnauthorized: false },
+  },
+  pool: {
+    min: 2,
+    max: 10,
+  },
+  migrations: {
+    tableName: "knex_migrations",
+  },
 };
